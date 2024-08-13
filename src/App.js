9af7596626280a47a94df89b644c7629e5f29c2a
Raphael Serafim <raphaelvserafim@gmail.com>
2024-08-12 21:45:30 -0400

    ✨ feat: toast

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { ToastContainer, } from 'react-toastify';
 import { FlowEditor } from '@theflow/components';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
   return (
     <DndProvider backend={HTML5Backend}>
       <FlowEditor />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+
     </DndProvider>
   );
 }

632887249116d25c0d2ad19c1e320d1e200ebe03
Raphael Serafim <raphaelvserafim@gmail.com>
2024-08-14 00:25:30 -0400

    ✨ feat:  get flow

--- src/app/pages/Flow.jsx
+++ src/app/pages/Flow.jsx
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Cookies from "js-cookie";
+
+// Definir um cookie
 
 import { FlowEditor, Sidebar } from '@theflow/components';
 import { Grid } from '@mui/material';
+import { FlowService } from '@theflow/services/flow';
 export function Flow() {
-  const { code } = useParams();
-  console.log({ code });
+  Cookies.set("xthex_xflx_xowx", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoxLCJpYXQiOjE3MjM1OTY3NjAsImV4cCI6MTcyNDIwMTU2MH0.jhdTdxHmwLalhRYBVC75HtZFJ1nLMG6Kr7TafTRECHw");
 
-  const savedNodes = localStorage.getItem('nodes');
+  const { code } = useParams();
+
+  const [savedNodes, setSavedNodes] = useState([]);
+
   const savedEdges = localStorage.getItem('edges');
+
+  const save = (data) => {
+    console.log({ data });
+    return new Promise((resolve, reject) => {
+
+      resolve(true);
+    });
+  };
+
+  React.useEffect(() => {
+    if (code) {
+      FlowService.fetchGetFlow(code).then(({ status, nodes }) => {
+        if (status === 200) {
+          if (nodes.length > 0) {
+            setSavedNodes(nodes);
+          }
+        }
+      });
+    }
+  }, [code]);
 
   return (
     <Grid style={{ display: 'flex', height: '100vh' }}>
       <Sidebar />
       <FlowEditor
-        nodes={savedNodes ? JSON.parse(savedNodes) : []}
+        code={code}
+        nodes={savedNodes}
         edges={savedEdges ? JSON.parse(savedEdges) : []}
-        save={(e) => console.log("save", e)}
+        save={save}
       />
     </Grid>
   )

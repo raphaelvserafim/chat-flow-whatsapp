25d33d1f0e41708c733697fe9e77be6c29f93fa9
Raphael Serafim <raphaelvserafim@gmail.com>
2024-08-15 22:16:00 -0400

    ✨ feat:  edit save text node

--- src/app/pages/Flow.jsx
+++ src/app/pages/Flow.jsx
@@ -1,21 +1,37 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Grid } from '@mui/material';
+import { Button, Grid, IconButton, InputAdornment, Popover, TextField } from '@mui/material';
 
 import Cookies from "js-cookie";
 
 import environment from '@theflow/configs/environment';
 import { FlowEditor, Sidebar } from '@theflow/components';
 import { FlowService } from '@theflow/services/flow';
+import { toast } from 'react-toastify';
+import { Modal } from '@theflow/components/Modal';
+import EmojiPicker from 'emoji-picker-react';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+
+Cookies.set(environment.COOKIES.SESSION, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoxLCJpYXQiOjE3MjM1OTY3NjAsImV4cCI6MTcyNDIwMTU2MH0.jhdTdxHmwLalhRYBVC75HtZFJ1nLMG6Kr7TafTRECHw");
 
 
 export function Flow() {
-
-  Cookies.set(environment.COOKIES.SESSION, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoxLCJpYXQiOjE3MjM1OTY3NjAsImV4cCI6MTcyNDIwMTU2MH0.jhdTdxHmwLalhRYBVC75HtZFJ1nLMG6Kr7TafTRECHw");
-
   const { code } = useParams();
   const [savedNodes, setSavedNodes] = useState([]);
   const [savedEdges, setSavedEdges] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [dataNodes, setDataNodes] = useState(null);
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [textMessage, setTextMessage] = useState(null);
+  const emojiAnchorRef = useRef(null);
+
+  const toggleEmojiPicker = () => {
+    setShowEmojiPicker(!showEmojiPicker);
+  };
+
+  const onEmojiClick = (event) => {
+    setTextMessage((prevText) => prevText + event.emoji);
+  };
 
 
   const save = async (data) => {
@@ -30,9 +46,10 @@ export function Flow() {
             y: data.position.y,
           },
         });
-        return response.status === 200;
+        return response;
       } catch (error) {
         console.error(error);
+        toast.error(error.message);
         return false;
       }
     }
@@ -45,9 +62,10 @@ export function Flow() {
             y: data.node.position.y,
           },
         });
-        return response.status === 200;
+        return response;
       } catch (error) {
         console.error(error);
+        toast.error(error.message);
         return false;
       }
     }
@@ -55,9 +73,11 @@ export function Flow() {
     if (data?.type === "remove") {
       try {
         const response = await FlowService.deleteNodes(code, data.node.id);
-        return response.status === 200;
+        console.log(response)
+        return response;
       } catch (error) {
         console.error(error);
+        toast.error(error.message);
         return false;
       }
     }
@@ -65,9 +85,10 @@ export function Flow() {
     if (data?.type === "connect") {
       try {
         const response = await FlowService.connectEdges(code, data.params);
-        return response.status === 200;
+        return response;
       } catch (error) {
         console.error(error);
+        toast.error(error.message);
         return false;
       }
     }
@@ -75,22 +96,56 @@ export function Flow() {
     if (data?.type === "deleteEdge") {
       try {
         const response = await FlowService.deleteEdges(code, data?.source, data?.target);
-        return response.status === 200;
+        return response;
       } catch (error) {
         console.error(error);
+        toast.error(error.message);
         return false;
       }
     }
-
-    return false;
   };
 
+
+  const handleEdit = (id) => {
+    setOpenModal(true);
+    const node = savedNodes.find((e) => e.id === id);
+    if (node) {
+      setTextMessage(node?.text_content);
+      setDataNodes(node);
+    }
+  }
+
+  const onChangetextMessage = (event) => {
+    setTextMessage(event.target.value);
+  }
+
+  const saveText = async () => {
+    try {
+      const { id } = dataNodes;
+      const { status, message } = await FlowService.updateContentNodes(code, id, {
+        text_content: textMessage
+      });
+      if (status === 200) {
+        toast.success(message);
+        const updatedNodes = savedNodes.map((node) =>
+          node.id === id ? { ...node, text_content: textMessage } : node
+        );
+        setSavedNodes(updatedNodes);
+      }
+
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+      return false;
+    }
+  }
 
   React.useEffect(() => {
     if (code) {
       FlowService.fetchGetFlow(code).then(({ status, nodes, edges }) => {
         if (status === 200) {
           if (nodes?.length > 0) {
+
             setSavedNodes(nodes);
           }
           if (edges?.length > 0) {
@@ -109,7 +164,56 @@ export function Flow() {
         nodes={savedNodes}
         edges={savedEdges}
         save={save}
+        onEdit={handleEdit}
       />
+
+      <Modal title="Editar" open={openModal} onClose={() => setOpenModal(false)}>
+        {dataNodes && dataNodes?.type === "text_message" && (
+          <Grid container padding={2} spacing={2}>
+            <Grid item md={12} mb={2}>
+              <TextField
+                fullWidth
+                multiline
+                autoComplete="off"
+                value={textMessage}
+                label="Texto"
+                variant="outlined"
+                onChange={onChangetextMessage}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        ref={emojiAnchorRef}
+                        onClick={toggleEmojiPicker}
+                      >
+                        <EmojiEmotionsIcon />
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Popover
+              open={showEmojiPicker}
+              anchorEl={emojiAnchorRef.current}
+              onClose={() => setShowEmojiPicker(false)}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              transformOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+            >
+              <EmojiPicker onEmojiClick={onEmojiClick} />
+            </Popover>
+            <Grid item md={12} mb={2}>
+              <Button color="success" variant="outlined" onClick={saveText}>Salvar</Button>
+            </Grid>
+          </Grid>
+        )}
+      </Modal>
     </Grid>
   )
 
